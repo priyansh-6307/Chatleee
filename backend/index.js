63c6737a0cf68ef31c5f08a088729684d6c38d96
priyansh-6307 <priyanshyadav364@gmail.com>
2025-01-22 22:58:05 +0530

    changes in the origin webste

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,7 @@ const server = app.listen(PORT, () => {
 
 
 const io= new Server(server,{
-  cors:{origin:"http://localhost:5173"}
+  cors:{origin:"https://eclectic-sunburst-8d5ecf.netlify.app/"}
 })
 
 io.on("connection", (socket) => {
